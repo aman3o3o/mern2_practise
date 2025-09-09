ee27e9985911a79608fd305566747e9bd61922be
Aman Prasad <amanpd14150518@gmail.com>
2025-09-09 15:08:16 +0530

    update mern2 practise

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,109 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+//third party middleware
+app.use(express.json());
+
 // file imports
 const { start_todo } = require("./db_connection/todo_dbconnection.js");
 const { todo_model } = require("./model/todo_model.js");
 
 // routes
-app.use("/insert-dataOne", async (req, res) => {
+app.post("/insert-dataOne", async (req, res) => {
     try {
-        const user = new todo_model({ "name": "A B", "email": "abcd@gmail.com", "number": "12345", "dob": "30/11/2001", "age": "21" });
-        await user.save();
-        if (user){
-            return res.status(200).json({
-                success : true,
-                message : "new user added successfully"
+        let { name, email, number, dob, age } = req.body;
+
+        let duplicate_email = await todo_model.findOne({ email });
+        if (duplicate_email) {
+            return res.status(409).json({
+                message: "email id already exist"
             })
         }
-        else{
+        let duplicate_number = await todo_model.findOne({ number });
+        if (duplicate_number) {
+            return res.status(409).json({
+                message: "number already exist"
+            })
+        }
+        let insert_data = { name, email, number, dob, age };
+        const new_user = new todo_model(insert_data);
+        await new_user.save();
+        if (new_user) {
+            return res.status(200).json({
+                success: true,
+                message: "new user added successfully"
+            })
+        }
+        else {
             return res.status(410).json({
-                success:false,
-                message:"user not added"
+                success: false,
+                message: "user not added"
             })
         }
     }
-    catch(err){
-        return res.status(500).send("internal server error (server side insert-dataOne route )");
+    catch (err) {
+        console.log("internal server error (server side read-dataAll)");
+        return res.status(500).json({
+            err: err
+        })
+    }
+})
+
+app.get("/read-dataAll", async (req, res) => {
+    try {
+        let user_data = await todo_model.find();
+        if (user_data.length <= 0) {
+            return res.status(404).json({
+                message: "user not found"
+            })
+        }
+        else {
+            return res.status(200).json({
+                success: true,
+                user_data: user_data
+            })
+        }
+    }
+    catch (err) {
+        console.log("internal server error (server side read-dataAll)");
+        return res.status(500).json({
+            err: err
+        })
+    }
+})
+
+app.delete("/delete-dataOne/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        if (!mongoose.Types.ObjectID.isvalid("id")) {
+            return res.status(404).json({
+                message: "id is not valid"
+            })
+        }
+        else {
+            return res.status(400).json({
+                message: "id not found"
+            })
+        }
+        let delete_id = { _id: ObjectID('id') }
+        let delete_data = await todo_model.deleteOne(delete_id);
+        if (delete_data.deletedCount) {
+            return res.status(200).json({
+                success: true,
+                message: "data deleted successfully"
+            })
+        }
+        else {
+            return res.status(500).json({
+                message: "data not deleted"
+            })
+        }
+    }
+    catch (err) {
+        console.log("internal server error (server side delete-dataOne)");
+        res.status(500).json({
+            err:err
+        })
     }
 })
 // server start function
