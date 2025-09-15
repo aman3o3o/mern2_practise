8b09ac0663f30f978fc1000cdd9019f29810ad34
Aman Prasad <amanpd14150518@gmail.com>
2025-09-15 21:47:19 +0530

    updated mern2 practise

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import Inputform from './Component/Inputform'
 import Loginform from './Component/Loginform'
 import Signupform from './Component/Signupform'
-import { Routes , Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { Navigate } from 'react-router-dom'
+import Error from "./Component/Error"
+import Data from "./Component/Data"
+import { useState } from "react"
+import WrongRoute from "./Component/WrongRoute"
+import RefreshHandler from './Component/RefreshHandler'
 
 const App = () => {
+
+  const [isauthenticated, setisauthenticated] = useState(localStorage.getItem('token'));
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login"/>}/>
-      <Route path="/login" element={<Loginform/>}/>
-      <Route path="signup" element={<Signupform/>}/>
-    </Routes>
+    {isauthenticated ? <RefreshHandler/> : null}
+      <Routes>
+        <Route path="/" element={isauthenticated ? <Navigate to = "/data"/> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Loginform />} />
+        <Route path="/signup" element={<Signupform />} />
+        <Route path="/data" element={isauthenticated ? <Data /> : <Error />} />
+        <Route path="*" element={<WrongRoute/>}/>
+      </Routes>
     </>
   )
 }
